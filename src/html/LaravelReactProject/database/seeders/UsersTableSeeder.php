4238761fea842b956c9d4a74a077a9aed36750a7
kyohei-p <ysdkyh11@gmail.com>
2024-03-22 07:15:36 +0900

    Usersテーブルのseed作成（ログイン用）

---
+++ src/html/LaravelReactProject/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'sample1',
+            'email' => 'sample1@example.com',
+            'password' => 'password1',
+        ]);
+    }
+}
